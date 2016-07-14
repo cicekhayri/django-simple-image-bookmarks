3d423f59e2c25496c0dd1c03b10cc4ab6bcb91cc
Hayri Cicek <hello@hayricicek.se>
2016-07-14 09:32:31 +0200

    created password reset templates

--- account/urls.py
+++ account/urls.py
@@ -12,4 +12,11 @@ urlpatterns = [
 	url(r'^logout-then-login/$',
 		'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
 	url(r'^$', views.dashboard, name='dashboard'),
+
+	# change password urls
+	url(r'^password-change/$',
+		'django.contrib.auth.views.password_change', name='password_change'),
+	url(r'^password-change/done/$',
+		'django.contrib.auth.views.password_change_done', name='password_change_done'),
+	
 ]
