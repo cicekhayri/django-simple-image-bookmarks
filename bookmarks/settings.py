65cf0968d6c8ac84fac1d534dc8c076bfbb62d90
Hayri Cicek <hello@hayricicek.se>
2016-07-14 11:11:31 +0200

    added custom authentication to login with username or email

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -103,6 +103,12 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+)
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
